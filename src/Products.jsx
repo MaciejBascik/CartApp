b62eed2a6ac663cbffd62003f9ff83268924a28d
MaciejBascik <maciej.b4scik@gmail.com>
2023-12-10 21:30:01 +0100

    Update Products.jsx

--- src/Products.jsx
+++ src/Products.jsx
@@ -45,7 +45,7 @@ export default function Products() {
   return (
     <div className=" w-full max-w-4xl mx-auto flex flex-col items-center justify-center">
 
-      <h1 className="text-[3em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Iphone 10</h1>
+      <h1 className="text-[2em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Iphone 10</h1>
       <div className="flex items-center justify-center">
       <button className="hover:border-[#646cff] outline-[4px_auto_-webkit-focus-ring-color] rounded-lg px-[1.5em] py-[0.4em] mx-[0.5em] text-[1em]  border-[#4a4fb8] border-[2px] [font-family:inherit] bg-[#1a1a1a] cursor-pointer [transition:border-color_0.25s]"
         onClick={() => dispatch({ type: "add", product: "amountIphone" })}
@@ -60,7 +60,7 @@ export default function Products() {
       </button>
       </div>
 
-      <h1 className="text-[3em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Samsung galaxy</h1>
+      <h1 className="text-[2em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Samsung galaxy</h1>
       <div className="flex items-center justify-center">
       <button className="hover:border-[#646cff] outline-[4px_auto_-webkit-focus-ring-color] rounded-lg px-[1.5em] py-[0.4em] mx-[0.5em] text-[1em]  border-[#4a4fb8] border-[2px] [font-family:inherit] bg-[#1a1a1a] cursor-pointer [transition:border-color_0.25s]"
         onClick={() => dispatch({ type: "add", product: "amountSamsung" })}
@@ -73,7 +73,7 @@ export default function Products() {
         -
       </button>
       </div>
-      <h1 className="text-[3em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Macbook</h1>
+      <h1 className="text-[2em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Macbook</h1>
       <div className="flex items-center justify-center">
       <button className="hover:border-[#646cff] outline-[4px_auto_-webkit-focus-ring-color] rounded-lg px-[1.5em] py-[0.4em] mx-[0.5em] text-[1em]  border-[#4a4fb8] border-[2px] [font-family:inherit] bg-[#1a1a1a] cursor-pointer [transition:border-color_0.25s]" onClick={() => dispatch({ type: "add", product: "amountMac" })}>
         +
@@ -84,7 +84,7 @@ export default function Products() {
         -
       </button>
       </div>
-      <h1 className="text-[3em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Airpods</h1>
+      <h1 className="text-[2em] animate-text bg-gradient-to-r from-[#ffffff] via-[#c7c7c7] to-[#aeaeae] bg-clip-text text-transparent font-semibold">Airpods</h1>
       <div className="flex items-center justify-center">
       <button className="hover:border-[#646cff] outline-[4px_auto_-webkit-focus-ring-color] rounded-lg px-[1.5em] py-[0.4em] mx-[0.5em] text-[1em]  border-[#4a4fb8] border-[2px] [font-family:inherit] bg-[#1a1a1a] cursor-pointer [transition:border-color_0.25s]" onClick={() => dispatch({ type: "add", product: "amountAir" })}>
         +

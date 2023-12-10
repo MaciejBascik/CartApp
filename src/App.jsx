311f4ba90775e3506ed4b17897cfdb39d93c4b2f
MaciejBascik <maciej.b4scik@gmail.com>
2023-12-10 21:28:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="font-[Inter,_system-ui,_Avenir,_Helvetica,_Arial,_sans-serif] text-[rgba(255,_255,_255,_0.87)] bg-[#242424] ">
       <div className="m-0 flex place-items-center min-w-[320px] min-h-screen">
+        <h1>Shopping App</h1>
       <Products />
       </div>
     </div>
